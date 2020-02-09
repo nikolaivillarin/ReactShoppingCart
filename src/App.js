5b862962391c1e71dbbf5d0edb776b4e3be3cabb
Nikolai Villarin <nikolai.villarin@gmail.com>
2020-02-09 07:38:07 -0500

    Completed add to cart functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from './logo.svg'
 import NavBar from './Navbar'
 import Home from './Home'
+import Cart from './Cart'
 import { Switch, BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <NavBar />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/cart" component={Cart} />
         </Switch>
       </div>
     </BrowserRouter>
